8dcfea87b6c16c1032aebae3ad44a9d0c074af9e
Ze Gan <ganze718@gmail.com>
2020-12-08 01:56:15 +0800

    Update stun server
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- examples/peerconnection/serverless/defaults.cc
+++ examples/peerconnection/serverless/defaults.cc
@@ -42,7 +42,7 @@ std::string GetEnvVarOrDefault(const char* env_var_name,
 }
 
 std::string GetPeerConnectionString() {
-  return GetEnvVarOrDefault("WEBRTC_CONNECT", "stun:stun.l.google.com:19302");
+  return GetEnvVarOrDefault("WEBRTC_CONNECT", "stun:stun.opennetlab.org:20000");
 }
 
 std::string GetDefaultServerName() {
