cdd7a1e3a30719feb750affb21a6d6a9c6e31249
Zhongyang Xia <v-zhonxia@microsoft.com>
2020-12-22 12:04:32 +0000

    Enable debug log

--- examples/peerconnection/serverless/main.cc
+++ examples/peerconnection/serverless/main.cc
@@ -122,16 +122,18 @@ int main(int argc, char* argv[]) {
     exit(EINVAL);
   }
 
-  webrtc::field_trial::InitFieldTrialsFromString(
-      "WebRTC-KeepAbsSendTimeExtension/Enabled/");  //  Config for
-                                                    //  hasAbsSendTimestamp in
-                                                    //  RTP Header extension
-
   const auto json_file_path = argv[1];
   if (!webrtc::ParseAlphaCCConfig(json_file_path)) {
     perror("bad config file");
     exit(EINVAL);
   }
+
+  rtc::LogMessage::LogToDebug(rtc::LS_INFO);
+
+  webrtc::field_trial::InitFieldTrialsFromString(
+      "WebRTC-KeepAbsSendTimeExtension/Enabled/");  //  Config for
+                                                    //  hasAbsSendTimestamp in
+                                                    //  RTP Header extension
 
 #ifdef WIN32
   rtc::WinsockInitializer win_sock_init;
