99a085abb7b3b965e6d82609aec28ce2e2a2bec2
Zhongyang Xia <v-zhonxia@microsoft.com>
2021-01-14 14:03:12 +0000

    Update FileLogSink to accept custom filepath

--- examples/peerconnection/serverless/logger.cc
+++ examples/peerconnection/serverless/logger.cc
@@ -1,23 +1,33 @@
-#include <iostream>
 #include "logger.h"
+#include <iostream>
+
+FileLogSink::FileLogSink(std::string log_filepath)
+    : log_filepath_(log_filepath) {
+  log_file_ = fopen(log_filepath_.c_str(), "a");
+  if (NULL == log_file_) {
+    return;
+  }
+
+  rtc::LogMessage::AddLogToStream(this, rtc::LoggingSeverity::INFO);
+}
+
+FileLogSink::~FileLogSink() {
+  fclose(log_file_);
+  rtc::LogMessage::RemoveLogToStream(this);
+}
 
 void FileLogSink::OnLogMessage(const std::string& msg,
-                           rtc::LoggingSeverity severity,
-                           const char* tag) {
+                               rtc::LoggingSeverity severity,
+                               const char* tag) {
   OnLogMessage(tag + (": " + msg), severity);
 }
 
 void FileLogSink::OnLogMessage(const std::string& msg,
-                           rtc::LoggingSeverity /* severity */) {
+                               rtc::LoggingSeverity /* severity */) {
   OnLogMessage(msg);
 }
 
 void FileLogSink::OnLogMessage(const std::string& message) {
-  FILE* log_file_ = fopen(LOG_FILEPATH.c_str(), "a");
-  if (NULL == log_file_) {
-    return;
-  }
   fwrite(message.c_str(), message.length(), 1, log_file_);
   fflush(log_file_);
-  fclose(log_file_);
 }
--- examples/peerconnection/serverless/logger.h
+++ examples/peerconnection/serverless/logger.h
@@ -1,6 +1,9 @@
 #include "rtc_base/logging.h"
 
 class FileLogSink : public rtc::LogSink {
+ public:
+  FileLogSink(std::string log_filepath);
+  ~FileLogSink();
   void OnLogMessage(const std::string& message);
   void OnLogMessage(const std::string& msg,
                     rtc::LoggingSeverity severity,
@@ -9,5 +12,6 @@ class FileLogSink : public rtc::LogSink {
                     rtc::LoggingSeverity /* severity */);
 
  private:
-  std::string LOG_FILEPATH = "webrtc.log";
+  std::string log_filepath_;
+  FILE* log_file_;
 };
