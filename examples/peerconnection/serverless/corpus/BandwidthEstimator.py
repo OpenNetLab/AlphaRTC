3fcdf80a31f0be16eddf39b901bb31bbd85b9f19
Ze Gan <ganze718@gmail.com>
2021-03-25 12:18:46 +0800

    Update Readme
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- examples/peerconnection/serverless/corpus/BandwidthEstimator.py
+++ examples/peerconnection/serverless/corpus/BandwidthEstimator.py
@@ -1,6 +1,19 @@
 
 class Estimator(object):
     def report_states(self, stats: dict):
+        '''
+        stats is a dict with the following items
+        {
+            "send_time_ms": uint,
+            "arrival_time_ms": uint,
+            "payload_type": int,
+            "sequence_number": uint,
+            "ssrc": int,
+            "padding_length": uint,
+            "header_length": uint,
+            "payload_size": uint
+        }
+        '''
         pass
 
     def get_estimated_bandwidth(self)->int:
