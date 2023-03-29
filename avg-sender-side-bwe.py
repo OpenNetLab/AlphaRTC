a901e19cca5dbf0b283e8f2b91cd213908ee315b
Jeongyoon Eo <Jeongyoon Eo>
2023-03-29 13:41:07 +0900

    p90 and p99 bwe produced by BitrateAllocator

--- avg-sender-side-bwe.py
+++ avg-sender-side-bwe.py
@@ -1,5 +1,6 @@
 from statistics import mean, median
 import argparse
+import numpy as np
 
 def main(args):
     filename = args.filename
@@ -7,7 +8,15 @@ def main(args):
     with open(filename, 'r') as f:
         lines = [float(line.rstrip()) for line in f]
 
-    print(f'BWE: average {mean(lines)/1000000} Mbps, median {median(lines)/1000000} Mbps')
+    a = np.array(lines)
+    p50 = np.percentile(a, 50) / 1000000 # return 50th percentile, e.g median.
+    p90 = np.percentile(a, 90) / 1000000 # return 50th percentile, e.g median.
+    p99 = np.percentile(a, 99) / 1000000 # return 50th percentile, e.g median.
+
+    average = mean(lines)/1000000
+    med = median(lines)/1000000
+
+    print(f'BWE: average {average} median {med} p90 {p90} p99 {p99} Mbps')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
