ee6082ab3dbb42766c90073fb68281fab5eb6ee0
Jeongyoon Eo <Jeongyoon Eo>
2023-03-29 19:22:20 +0900

    plot sender-side bwe produced by CC

---
+++ plot-bwe.py
@@ -0,0 +1,52 @@
+from statistics import mean, median
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_log():
+    bwe_l = []
+    with open('webrtc_sender.log', 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            if 'Current BWE' in line:
+                l = line.split()
+                bwe_l.append(l[3])
+
+    return bwe_l
+
+def main():
+    bwe_l_bps = parse_log()
+    bwe_l = []
+    for line in bwe_l_bps:
+        bwe_l.append(float(line.rstrip())/1000000) # Mbps
+
+    os.remove('gcc-bwe-file')
+    for line in bwe_l:
+        with open('gcc-bwe-file', 'a') as f:
+            f.write(str(line)+'\n')
+
+    a = np.array(bwe_l)
+    average = "{:.2f}".format(mean(bwe_l))
+    med = "{:.2f}".format(median(bwe_l))
+    p90 = "{:.2f}".format(np.percentile(a, 90)) # 90th percentile
+    p99 = "{:.2f}".format(np.percentile(a, 99)) # 99th percentile
+
+    title = f'RL-based CC BWE: avg {average} med {med} p90 {p90} p99 {p99} Mbps'
+
+    x_axis = []
+    len_bwe_l = len(bwe_l)
+    for i in range(0, len_bwe_l):
+        # bwe logged at every 100ms
+        x_axis.append(i * 100)
+    y_axis = bwe_l
+
+    plt.plot(x_axis, y_axis)
+    plt.title(title)
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Estimated Bandwidth (Mbps)')
+    plt.savefig('rlcc-bwe.pdf')
+
+
+if __name__ == "__main__":
+    main()
