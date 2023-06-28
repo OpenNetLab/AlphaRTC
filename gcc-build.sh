468bae74271f0158b0d92f327e2488c3f9495aa1
Jeongyoon Eo <jeongyoon.eo@snu.ac.kr>
2023-06-28 19:14:31 +0900

    gcc-build.sh for docker-free build

---
+++ gcc-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -d "out" ]
+then
+	mkdir out
+	mkdir out/Default
+	# Do gclient sync only for initial build,
+	# as it takes a long time to do for every build
+	gclient sync
+	mv -fvn src/* .
+	rm -rf src
+	sudo apt install ninja-build
+fi
+
+
+# Build AlphaRTC.
+gn gen out/Default --args='is_debug=false'
+
+# Build AlphaRTC e2e app that uses GCC (peerconnection_serverless.gcc).
+ninja -C out/Default peerconnection_challenge_client
+cp out/Default/peerconnection_challenge_client peerconnection_serverless_gcc
